547114de0bcd5f7dd4b60bfbcd6fe0aa696c8bbd
Hugo PANEL <38533398+hugopanel@users.noreply.github.com>
2025-01-16 12:01:24 +0100

    Removed role from update requests (#7)

--- src/formats/CompanyRequests.ts
+++ src/formats/CompanyRequests.ts
@@ -4,7 +4,10 @@ import { InferInsertModel } from "drizzle-orm";
 export interface UpdateCompanyRequest
   extends Partial<Omit<InferInsertModel<typeof companyUsersTable>, "user">>,
     Partial<
-      Omit<InferInsertModel<typeof usersTable>, "created_at" | "modified_at">
+      Omit<
+        InferInsertModel<typeof usersTable>,
+        "created_at" | "modified_at" | "role"
+      >
     > {
   id: number;
 }
